6676a283596b81e1335d801f4e975e925678304e
caoweili <2674754050@qq.com>
2022-07-06 21:26:45 +0800

    优化串口相关

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,141 +1,137 @@
 <template>
-  <div class="com_container">
-    <!-- <Header /> -->
-    <div class="home_c">
-      <div class="home_left shadow-card">
-        <div>
-          <span>串口号</span>
-          <a-select class="home_input" style="width: 120px" @change="handleChange" :disabled="open" defaultValue="COM1">
-            <a-select-option v-for="(item, index) in ports" :key="index" :value="item.path">{{ item.path }}</a-select-option>
-          </a-select>
-        </div>
-        <div>
-          <span>波特率</span>
-          <a-select class="home_input" style="width: 120px" :disabled="open" :defaultValue="9600" @change="handleChangeBaudRage">
-            <a-select-option v-for="item in serialConfig.baudRate" :key="item" :value="item">{{ item }}</a-select-option>
-          </a-select>
-        </div>
-        <div>
-          <span>校验位</span>
-          <a-select class="home_input" style="width: 120px" :disabled="open" defaultValue="none" @change="handleChangeParity">
-            <a-select-option v-for="item in serialConfig.parity" :key="item" :value="item">{{ item }}</a-select-option>
-          </a-select>
-        </div>
+	<div class="com_container">
+		<!-- <Header /> -->
+		<div class="home_c">
+			<div class="home_left shadow-card">
+				<div>
+					<span>串口号</span>
+					<a-select class="home_input" style="width: 120px" :disabled="open" v-model:value="port">
+						<a-select-option v-for="(item, index) in ports" :key="index" :value="item.path">{{ item.path }}</a-select-option>
+					</a-select>
+				</div>
+				<div>
+					<span>波特率</span>
+					<a-select class="home_input" style="width: 120px" :disabled="open" v-model:value="option.baudRate">
+						<a-select-option v-for="item in serialConfig.baudRate" :key="item" :value="item">{{ item }}</a-select-option>
+					</a-select>
+				</div>
+				<div>
+					<span>校验位</span>
+					<a-select class="home_input" style="width: 120px" :disabled="open" defaultValue="none" v-model:value="option.parity">
+						<a-select-option v-for="item in serialConfig.parity" :key="item" :value="item">{{ item }}</a-select-option>
+					</a-select>
+				</div>
 
-        <div>
-          <span>数据位</span>
-          <a-select class="home_input" style="width: 120px" :disabled="open" :defaultValue="8" @change="handleChangeDataBits">
-            <a-select-option v-for="item in serialConfig.dataBits" :key="item" :value="item">{{ item }}bit</a-select-option>
-          </a-select>
-        </div>
-        <div>
-          <span>停止位</span>
-          <a-select class="home_input" style="width: 120px" :disabled="open" :defaultValue="1" @change="handleChangeStopBits">
-            <a-select-option v-for="item in serialConfig.stopBits" :key="item" :value="item">{{ item }}bit</a-select-option>
-          </a-select>
-        </div>
-        <div>
-          <span>开/关</span>
-          <a-switch :checked="open" @change="handleSwitch" />
-        </div>
+				<div>
+					<span>数据位</span>
+					<a-select class="home_input" style="width: 120px" :disabled="open" :defaultValue="8" v-model:value="option.dataBits">
+						<a-select-option v-for="item in serialConfig.dataBits" :key="item" :value="item">{{ item }}bit</a-select-option>
+					</a-select>
+				</div>
+				<div>
+					<span>停止位</span>
+					<a-select class="home_input" style="width: 120px" :disabled="open" :defaultValue="1" v-model:value="option.stopBits">
+						<a-select-option v-for="item in serialConfig.stopBits" :key="item" :value="item">{{ item }}bit</a-select-option>
+					</a-select>
+				</div>
+				<div>
+					<span>开/关</span>
+					<a-switch v-model:checked="open" />
+				</div>
 
-        <div>
-          <a-divider>接收区设置</a-divider>
-          <div class="home_check">
-            <a-checkbox @change="handleHexDisplay">十六进制显示</a-checkbox>
-          </div>
-        </div>
-      </div>
-      <div class="home_right">
-        <div class="home_text shadow-card" ref="scrollRef">
-          <div v-for="(item, index) in msg" :key="index" :class="item.chat === 'roboto' ? 'right_msg' : 'left_msg'">
-            <div v-if="item.chat=='roboto'" style="display:flex;align-items:center">
-              <a-avatar shape="square" :size="32">
-                R
-              </a-avatar>
-              <div class="msgContent">收→{{ item.content }}</div>
-            </div>
-            <div v-else class="msgContent">{{ item.chat ?? '发' }}→{{ item.content }}</div>
-            <!-- {{ item.content }} -->
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="home_b">
-      <div class="home_sendConfig">
-        <div class="home_check" style="margin-bottom: 12px;">
-          <a-checkbox @change="handleHexSend">十六进制发送</a-checkbox>
-        </div>
-        <div class="home_check">
-          <a-checkbox @change="openInterval">定时发送</a-checkbox>
-          <a-input-number :disabled="isInterval" v-model="time" :defaultValue="time" :min="1" :max="99999" @change="handleSetInterval" />
-        </div>
-      </div>
-      <div class="home_send">
-        <a-textarea class="home_send_input" placeholder="请输入要发送的信息" @change="handleSendValue" allowClear autoSize />
-        <div class="home_btn">
-          <a-button type="primary" class="size-btn" :disabled="!open" @click="handleSendMsg">发送</a-button>
-          <a-button type="primary" class="size-btn" danger @click="clearMsg">清空</a-button>
-        </div>
-      </div>
-    </div>
-  </div>
+				<div>
+					<a-divider>接收区设置</a-divider>
+					<div class="home_check">
+						<a-checkbox @change="handleHexDisplay">十六进制显示</a-checkbox>
+					</div>
+				</div>
+			</div>
+			<div class="home_right">
+				<div class="home_text shadow-card" ref="scrollRef">
+					<div v-for="(item, index) in msg" :key="index" :class="item.chat === 'roboto' ? 'right_msg' : 'left_msg'">
+						<div v-if="item.chat == 'roboto'" style="display:flex;align-items:center">
+							<a-avatar shape="square" :size="32">
+								R
+							</a-avatar>
+							<div class="msgContent">收→{{ item.content }}</div>
+						</div>
+						<div v-else class="msgContent">{{ item.chat ?? '发' }}→{{ item.content }}</div>
+						<!-- {{ item.content }} -->
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="home_b">
+			<div class="home_sendConfig">
+				<div class="home_check" style="margin-bottom: 12px;">
+					<a-checkbox @change="handleHexSend">十六进制发送</a-checkbox>
+				</div>
+				<div class="home_check">
+					<a-checkbox @change="openInterval">定时发送</a-checkbox>
+					<a-input-number :disabled="isInterval" v-model="time" :defaultValue="time" :min="1" :max="99999" @change="handleSetInterval" />
+				</div>
+			</div>
+			<div class="home_send">
+				<a-textarea class="home_send_input" placeholder="请输入要发送的信息" @change="handleSendValue" allowClear autoSize />
+				<div class="home_btn">
+					<a-button type="primary" class="size-btn" :disabled="!open" @click="handleSendMsg">发送</a-button>
+					<a-button type="primary" class="size-btn" danger @click="clearMsg">清空</a-button>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 <script setup>
-import { onBeforeMount, ref, getCurrentInstance, watch, nextTick } from 'vue';
-// import Header from '@/components/Header';
+import { onBeforeMount, ref, getCurrentInstance, watch, nextTick, computed, onBeforeUnmount } from 'vue';
+import { useComStore } from '@/store';
+import { storeToRefs } from 'pinia';
 import serialport from 'serialport';
-import serialConfig from '@/config';
+import { serialConfig } from '@/config';
 // import robots from '@/assets/robots.png';
 const currentInstance = getCurrentInstance();
 const { $message } = currentInstance.appContext.config.globalProperties;
+const comer = useComStore();
+let { open, value: port, option } = storeToRefs(comer);
 
 // 串口列表
 let ports = ref([]);
-// 串口号
-const port = ref('COM1');
-// 串口配置
-const option = {
-  baudRate: 9600, // 波特率
-  dataBits: 8, // 数据位
-  parity: 'none', // 校验位
-  stopBits: 1, // 停止位
-  flowControl: false
-};
-// 串口是否打开
-const open = ref(false);
 // 是否是十六进制显示
 let hexDisplay = false;
 // 是否是十六进制发送
 let hexSend = false;
+let comTimer = null;
 
 // 获取串口列表
 onBeforeMount(() => {
-  setInterval(() => {
-    serialport.list().then(list => {
-      ports.value = list;
-    });
-  }, 2000);
+	comTimer = setInterval(() => {
+		console.log('tttt');
+		serialport.list().then(list => {
+			ports.value = list;
+		});
+	}, 2000);
+});
+
+onBeforeUnmount(() => {
+	clearInterval(comTimer);
+	if (open.value && COM) COM.close(() => {});
 });
 
 const handleHexDisplay = e => {
-  hexDisplay = e.target.checked;
+	hexDisplay = e.target.checked;
 };
 
 const handleHexSend = e => {
-  hexSend = e.target.checked;
+	hexSend = e.target.checked;
 };
 // 修改串口
 const handleChange = value => {
-  port.value = value;
+	port.value = value;
 };
 
 // 串口
 let COM = null;
-
 // 消息
 const msg = ref([]);
-
 // 发送的信息
 let sendMsg = '';
 
@@ -145,109 +141,94 @@ let timer = ref(null);
 let isInterval = ref(false);
 
 const openInterval = e => {
-  isInterval.value = e.target.checked;
-  timer.value && window.clearInterval(timer.value);
-  if (e.target.checked) {
-    timer.value = window.setInterval(() => {
-      handleSendMsg();
-    }, time.value);
-  }
+	isInterval.value = e.target.checked;
+	timer.value && window.clearInterval(timer.value);
+	if (e.target.checked) {
+		timer.value = window.setInterval(() => {
+			handleSendMsg();
+		}, time.value);
+	}
 };
 const handleSetInterval = value => {
-  time.value = value;
+	time.value = value;
+};
+// 清空消息
+const clearMsg = () => {
+	msg.value = [];
 };
 // 开关串口
 const handleSwitch = checked => {
-  if (checked) {
-    COM = new serialport(port.value, option, false);
-    COM.on('error', function () {
-      $message.error('端口已经被占用');
-      open.value = false;
-    });
-    // 接受消息
-    COM.on('readable', () => {
-      let content = '';
+	console.log(checked);
+	if (checked) {
+		COM = new serialport(port.value, option.value, false);
+		COM.on('error', function() {
+			$message.error('端口已经被占用');
+			open.value = false;
+		});
+		// 接受消息
+		COM.on('readable', () => {
+			let content = '';
+			if (hexDisplay) {
+				COM.read().map(item => {
+					content += item.toString(16);
+				});
+			} else {
+				content = COM.read().toString();
+			}
 
-      if (hexDisplay) {
-        COM.read().map(item => {
-          content += item.toString(16);
-        });
-      } else {
-        content = COM.read().toString();
-      }
-
-      if (content === '\n') {
-        return;
-      }
-      msg.value.push({ chat: 'roboto', content });
-    });
-    open.value = true;
-  } else {
-    open.value = false;
-    COM.close(() => { });
-  }
+			if (content === '\n') {
+				return;
+			}
+			msg.value.push({ chat: 'roboto', content });
+		});
+		open.value = true;
+	} else {
+		open.value = false;
+		if (COM) COM.close(() => {});
+	}
 };
 
 // 设置发送消息
 const handleSendValue = e => {
-  sendMsg = e.currentTarget.value;
+	sendMsg = e.currentTarget.value;
 };
 function strToHexCharCode(str) {
-  if (str === '') return '';
-  var hexCharCode = [];
-  for (var i = 0; i < str.length; i += 2) {
-    if (i + 1 < str.length) {
-      hexCharCode.push(parseInt(`${str[i]}${str[i]}`, 16));
-    } else {
-      hexCharCode.push(parseInt(str[i], 16));
-    }
-  }
-  return hexCharCode;
+	if (str === '') return '';
+	var hexCharCode = [];
+	for (var i = 0; i < str.length; i += 2) {
+		if (i + 1 < str.length) {
+			hexCharCode.push(parseInt(`${str[i]}${str[i]}`, 16));
+		} else {
+			hexCharCode.push(parseInt(str[i], 16));
+		}
+	}
+	return hexCharCode;
 }
 // 发送消息
 const handleSendMsg = () => {
-  if (open.value) {
-    msg.value.push({ chat: 'me', content: sendMsg });
-    // COM.write(sendMsg);
-    if (hexSend) {
-      COM.write(new Uint8Array(strToHexCharCode(sendMsg)));
-    } else {
-      COM.write(sendMsg);
-    }
-  }
+	if (open.value) {
+		msg.value.push({ chat: 'me', content: sendMsg });
+		// COM.write(sendMsg);
+		if (hexSend) {
+			COM.write(new Uint8Array(strToHexCharCode(sendMsg)));
+		} else {
+			COM.write(sendMsg);
+		}
+	}
 };
 
-// 清空消息
-const clearMsg = () => {
-  msg.value = [];
-};
-
-// 修改波特率
-const handleChangeBaudRage = value => {
-  option.baudRate = value;
-};
-
-// 修改校验位
-const handleChangeParity = value => {
-  option.parity = value;
-};
-
-// 修改数据位
-const handleChangeDataBits = value => {
-  option.dataBits = value;
-};
-
-// 修改停止位
-const handleChangeStopBits = value => {
-  option.stopBits = value;
-};
 const scrollRef = ref(null);
 watch(
-  () => [...msg.value],
-  async () => {
-    await nextTick();
-    scrollRef.value.scrollTop = scrollRef.value.scrollHeight;
-  }
+	() => [...msg.value],
+	async () => {
+		await nextTick();
+		scrollRef.value.scrollTop = scrollRef.value.scrollHeight;
+	}
+);
+watch(
+	() => open.value,
+	() => handleSwitch(open.value),
+	{ immediate: true }
 );
 </script>
 <style scoped>
