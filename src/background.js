1cc00fd02bf5a068f1cd1e397d9e9094f57e1dfb
caoweili <2674754050@qq.com>
2022-07-06 16:49:52 +0800

    优化

--- src/background.js
+++ src/background.js
@@ -9,108 +9,109 @@ const isDevelopment = process.env.NODE_ENV !== 'production';
 protocol.registerSchemesAsPrivileged([{ scheme: 'app', privileges: { secure: true, standard: true } }]);
 let win;
 async function createWindow() {
-  // Create the browser window.
-  win = new BrowserWindow({
-    width: 930,
-    height: 635,
-    minWidth: 930,
-    minHeight: 635,
-    useContentSize: false,
-    frame: false,
-    show: false,
-    webPreferences: {
-      // Use pluginOptions.nodeIntegration, leave this alone
-      // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
-      nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION,
-      contextIsolation: !process.env.ELECTRON_NODE_INTEGRATION
-    }
-  });
+	// Create the browser window.
+	win = new BrowserWindow({
+		width: 930,
+		height: 635,
+		minWidth: 930,
+		minHeight: 635,
+		useContentSize: false,
+		frame: false,
+		show: true,
+		webPreferences: {
+			// Use pluginOptions.nodeIntegration, leave this alone
+			// See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
+			nodeIntegration: true,
+			enableRemoteModule: true,
+			contextIsolation: false
+		}
+	});
 
-  if (process.env.WEBPACK_DEV_SERVER_URL) {
-    // Load the url of the dev server if in development mode
-    await win.loadURL(process.env.WEBPACK_DEV_SERVER_URL);
-    if (!process.env.IS_TEST) win.webContents.openDevTools();
-  } else {
-    createProtocol('app');
-    // Load the index.html when not in development
-    win.loadURL('app://./index.html');
-  }
+	if (process.env.WEBPACK_DEV_SERVER_URL) {
+		// Load the url of the dev server if in development mode
+		await win.loadURL(process.env.WEBPACK_DEV_SERVER_URL);
+		if (!process.env.IS_TEST) win.webContents.openDevTools();
+	} else {
+		createProtocol('app');
+		// Load the index.html when not in development
+		win.loadURL('app://./index.html');
+	}
 
-  win.on('ready-to-show', function () {
-    win.show(); // 初始化后再显示
-  });
+	win.on('ready-to-show', function() {
+		win.show(); // 初始化后再显示
+	});
 }
 app.allowRendererProcessReuse = false;
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+	// On macOS it is common for applications and their menu bar
+	// to stay active until the user quits explicitly with Cmd + Q
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
 });
 
 app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+	// On macOS it's common to re-create a window in the app when the
+	// dock icon is clicked and there are no other windows open.
+	if (BrowserWindow.getAllWindows().length === 0) createWindow();
 });
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on('ready', async () => {
-  // if (isDevelopment && !process.env.IS_TEST) {
-  // 	// Install Vue Devtools
-  //   var vue_devtools_beta = { id: "ljjemllljcmogpfapbkkighbhhppjdbg", electron: ">=1.2.1" }
+	// if (isDevelopment && !process.env.IS_TEST) {
+	// 	// Install Vue Devtools
+	//   var vue_devtools_beta = { id: "ljjemllljcmogpfapbkkighbhhppjdbg", electron: ">=1.2.1" }
 
-  // 	try {
-  // 		await installExtension(vue_devtools_beta);
-  // 	} catch (e) {
-  // 		console.error('Vue Devtools failed to install:', e.toString());
-  // 	}
-  // }
-  createWindow();
+	// 	try {
+	// 		await installExtension(vue_devtools_beta);
+	// 	} catch (e) {
+	// 		console.error('Vue Devtools failed to install:', e.toString());
+	// 	}
+	// }
+	createWindow();
 });
 
 // Exit cleanly on request from parent process in development mode.
 if (isDevelopment) {
-  if (process.platform === 'win32') {
-    process.on('message', data => {
-      if (data === 'graceful-exit') {
-        app.quit();
-      }
-    });
-  } else {
-    process.on('SIGTERM', () => {
-      app.quit();
-    });
-  }
+	if (process.platform === 'win32') {
+		process.on('message', data => {
+			if (data === 'graceful-exit') {
+				app.quit();
+			}
+		});
+	} else {
+		process.on('SIGTERM', () => {
+			app.quit();
+		});
+	}
 }
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
 });
-ipcMain.on('window-close', function () {
-  win.close();
-});
-
-ipcMain.on('window-min', function () {
-  win.minimize();
+ipcMain.on('window-close', function() {
+	win.close();
 });
 
-ipcMain.on('window-max', function () {
-  if (win.isMaximized()) {
-    win.unmaximize();
-  } else {
-    win.maximize();
-  }
+ipcMain.on('window-min', function() {
+	win.minimize();
 });
-if (isDevelopment) {
-  try {
-    require('electron-reloader')(module);
-  } catch (err) {
-  }
-}
+
+ipcMain.on('window-max', function() {
+	if (win.isMaximized()) {
+		win.unmaximize();
+	} else {
+		win.maximize();
+	}
+});
+// if (isDevelopment) {
+//   try {
+//     require('electron-reloader')(module);
+//   } catch (err) {
+//   }
+// }
